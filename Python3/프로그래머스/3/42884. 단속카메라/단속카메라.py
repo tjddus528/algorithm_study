11bdd9a3b22e910af117c6c2d0bb231db9c3e5ce
Sungyeon <csy357@gmail.com>
2024-02-27 21:55:24 +0900

    [level 3] Title: 단속카메라, Time: 11.54 ms, Memory: 10.5 MB -BaekjoonHub

--- Python3/프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ Python3/프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,22 +1,3 @@
 def solution(routes):
     answer = 0
-    camera = []
-    checked = [False] * len(routes)
-    # 1. 시작지점 기준으로 정렬
-    routes.sort(key = lambda x : x[0])
-    camera.append(routes[0])
-    checked[0] = True
-    for i in range(1, len(routes)):
-        start = routes[i][0]
-        end = routes[i][1]
-        for c in camera:
-            if c[0] <= end and c[1] >= start:
-                c[0] = max(c[0], start)
-                c[1] = min(c[1], end)
-                checked[i] = True
-                break
-        if not checked[i]:
-            camera.append(routes[i])
-        # print(camera)
-        
-    return len(camera)
+    return answer

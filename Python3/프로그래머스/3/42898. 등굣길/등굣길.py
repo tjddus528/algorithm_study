2a5ebbe76e9c29c33198450484c5e382254156d2
Sungyeon <csy357@gmail.com>
2024-02-28 17:47:25 +0900

    [level 3] Title: 등굣길, Time: 4.00 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/3/42898. 등굣길/등굣길.py
+++ Python3/프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,21 +1,3 @@
 def solution(m, n, puddles):
     answer = 0
-    INF = int(1e9)
-    maps = [[0]*(m+1) for _ in range(n+1)]
-    
-    maps[1][1] = 1
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            if [j,i] in puddles:
-                continue
-            if i == 1 and j == 1:
-                continue
-            maps[i][j] = maps[i][j-1] + maps[i-1][j]
-            
-            # for a in range(1, n+1):
-            #     for b in range(1, m+1):
-            #         print(maps[a][b], end = ' ')
-            #     print()
-            # print()
-    answer = maps[n][m]
-    return answer%1000000007
+    return answer

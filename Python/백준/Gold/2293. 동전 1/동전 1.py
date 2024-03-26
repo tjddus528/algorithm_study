37b257ec486b42489d72477a2964fd5aa5b92324
Sungyeon <csy357@gmail.com>
2024-03-26 09:56:06 +0900

    [Gold V] Title: 동전 1, Time: 184 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Gold/2293. 동전 1/동전 1.py
+++ Python/백준/Gold/2293. 동전 1/동전 1.py
@@ -6,13 +6,9 @@ for i in range(n):
 d = [0] * (k+1)
 coins.sort()
 
-for i in range(coins[0], k+1, coins[0]):
-    d[i] = 1
-
-for cidx in range(1, n):
-    for i in range(coins[cidx]+1, k+1):
-        d[i] = d[i] + d[i-coins[cidx]]
-    for i in range(coins[cidx], k+1, coins[cidx]):
-        d[i] = d[i] + 1
+d[0] = 1
+for coin in coins:
+    for i in range(coin, k+1):
+        d[i] += d[i-coin]
 
 print(d[k])

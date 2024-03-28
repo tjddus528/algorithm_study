e2f111f41dfe7a5519d8c31dc0c281aab991436f
Sungyeon <csy357@gmail.com>
2024-03-28 12:09:18 +0900

    [Gold V] Title: 동전 2, Time: 428 ms, Memory: 31120 KB -BaekjoonHub

---
+++ Python/백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,23 @@
+n, k = map(int, input().split())
+coins = []
+for _ in range(n):
+    coins.append(int(input()))
+
+coins.sort()
+
+INF = int(1e9)
+d = [INF] * (k+1)
+for coin in coins:
+    if coin <= k:
+        d[coin] = 1
+
+for i in range(1, k+1):
+    for coin in coins:
+        if i > coin:
+            d[i] = min(d[i-coin]+1, d[i])
+
+result = d[k]
+if result >= INF:
+    print(-1)
+else:
+    print(result)

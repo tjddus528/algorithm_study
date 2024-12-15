498ec8d05da230e0287d587686783a5755463ddf
Sungyeon <csy357@gmail.com>
2024-12-15 18:13:43 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 148 ms, Memory: 35632 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.py
@@ -5,12 +5,10 @@ for _ in range(t):
     n, m = map(int, input().split())
     a = sorted(list(map(int, input().split())))
     b = sorted(list(map(int, input().split())))
-    cnt = 0
+    cnt, bidx = 0, 0
     for aa in a:
-        left, right = 0, len(b)-1
-        while left <= right:
-            mid = (left + right) // 2
-            if aa <= b[mid]: right = mid - 1
-            elif aa > b[mid]: left = mid + 1
-        cnt += len(b[:left])
+        while bidx < len(b):
+            if aa <= b[bidx]: break
+            bidx += 1
+        cnt += bidx
     print(cnt)

ee475307fbfc1726555cc4c404eb31ed668042fd
Sungyeon <csy357@gmail.com>
2024-12-20 00:45:57 +0900

    [Silver III] Title: 다중 일차 함수, Time: 464 ms, Memory: 75476 KB -BaekjoonHub

--- 백준/Silver/26258. 다중 일차 함수/다중 일차 함수.py
+++ 백준/Silver/26258. 다중 일차 함수/다중 일차 함수.py
@@ -2,18 +2,17 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 data = [list(map(int, input().split())) for _ in range(n)]
-
-def solution(k):
-    left, right = 0, n-1
-    while left <= right:
-        mid = (left + right)//2
-        if float(data[mid][0]) > k: right = mid -1
-        else: left = mid + 1
-
-    if data[left][1] > data[left-1][1]: return 1
-    elif data[left][1] < data[left-1][1]: return -1
-    else: return 0
-
 q = int(input())
-for _ in range(q):
-    print(solution(float(input())))
+k = list([i, float(input())] for i in range(q))
+k.sort(key=lambda x:x[1])
+
+i, j = 0, 0
+while i < n and j < q:
+    while float(data[i][0]) <= k[j][1]:
+        i += 1
+    if data[i][1] > data[i-1][1]: k[j].append(1)
+    elif data[i][1] < data[i-1][1]: k[j].append(-1)
+    else: k[j].append(0)
+    j += 1
+k.sort(key=lambda x:x[0])
+for kk in k: print(kk[2])

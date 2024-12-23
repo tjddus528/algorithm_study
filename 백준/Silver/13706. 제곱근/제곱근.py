a5efd1877d36557a8b8bd7aa52d36ac525215edf
Sungyeon <csy357@gmail.com>
2024-12-23 10:20:16 +0900

    [Silver IV] Title: 제곱근, Time: 40 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/13706. 제곱근/제곱근.py
+++ 백준/Silver/13706. 제곱근/제곱근.py
@@ -1,9 +1,2 @@
-n = int(input())
-left, right = 0, n
-while left <= right:
-    mid = (left + right) // 2
-    if mid**2 > n: right = mid - 1
-    elif mid**2 < n: left = mid + 1
-    else:
-        print(mid)
-        break
+import math
+print(math.isqrt(int(input())))

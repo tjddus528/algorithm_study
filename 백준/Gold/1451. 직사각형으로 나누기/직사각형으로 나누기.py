204959eb07262b9275b06b763be4e7eb96d24b80
Sungyeon <csy357@gmail.com>
2025-01-02 22:38:52 +0900

    [Gold IV] Title: 직사각형으로 나누기, Time: 76 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1451. 직사각형으로 나누기/직사각형으로 나누기.py
+++ 백준/Gold/1451. 직사각형으로 나누기/직사각형으로 나누기.py
@@ -1,29 +1,34 @@
 n, m = map(int, input().split())
-square = list(list(int(i) for i in input()) for _ in range(n))
+data = list(list(int(i) for i in input()) for _ in range(n))
+square = [[0]*m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        square[i][j] = sum(sum(item) for item in [data[k][:j+1] for k in range(i+1)])
+
 answer = 0
 # 1.
-for i in range(1, n):
-    for j in range(1, m):
-        a = sum(sum(item) for item in [square[k][:j] for k in range(i)])
-        b = sum(sum(item) for item in [square[k][j:] for k in range(i)])
-        c = sum(sum(item) for item in [square[k][:j] for k in range(i, n)])
-        d = sum(sum(item) for item in [square[k][j:] for k in range(i, n)])
-        answer = max(answer, max(a*b*(c+d), a*c*(b+d), (a+b)*c*d, (a+c)*b*d))
+for i in range(n):
+    for j in range(m):
+        a = square[i][j]
+        b = square[i][m-1] - square[i][j]
+        c = square[n-1][j] - square[i][j]
+        d = square[n-1][m-1] - (square[n-1][j] + square[i][m-1]) + square[i][j]
+        answer = max(answer, max(a * b * (c + d), a * c * (b + d), (a + b) * c * d, (a + c) * b * d))
 
-# 2. 세로 3등분
+# 2. 세로
 for j in range(m):
-    for jj in range(j, m):
-        a = sum(sum(item) for item in [square[k][:j] for k in range(n)])
-        b = sum(sum(item) for item in [square[k][j:jj] for k in range(n)])
-        c = sum(sum(item) for item in [square[k][jj:] for k in range(n)])
-        answer = max(answer, a*b*c)
-
-# 3. 가로 3등분
+    for jj in range(j+1, m):
+        a = square[n-1][j]
+        b = square[n-1][jj] - a
+        c = square[n-1][m-1] - a - b
+        answer = max(answer, a * b * c)
+# 3. 가로
 for i in range(n):
     for ii in range(i+1, n):
-        a = sum(sum(item) for item in [square[k][:] for k in range(i)])
-        b = sum(sum(item) for item in [square[k][:] for k in range(i, ii)])
-        c = sum(sum(item) for item in [square[k][:] for k in range(ii, n)])
-        answer = max(answer, a*b*c)
+        a = square[i][m-1]
+        b = square[ii][m-1] - a
+        c = square[n-1][m-1] - a - b
+        answer = max(answer, a * b * c)
 
 print(answer)

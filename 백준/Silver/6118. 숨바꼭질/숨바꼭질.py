f5b09674263ecc2aada977c88ad099ff1488b047
Sungyeon <csy357@gmail.com>
2025-01-08 13:23:37 +0900

    [Silver I] Title: 숨바꼭질, Time: 164 ms, Memory: 40696 KB -BaekjoonHub

--- 백준/Silver/6118. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/6118. 숨바꼭질/숨바꼭질.py
@@ -1,4 +1,6 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [[] for _ in range(n+1)]
 for _ in range(m):
@@ -7,17 +9,15 @@ for _ in range(m):
     graph[b].append(a)
 
 distance = [int(1e9) for _ in range(n+1)]
-visited = [0 for _ in range(n+1)]
+distance[1] = 0
 
 q = deque([1])
 distance[1] = 0
-visited[1] = 1
 while q:
     c = q.popleft()
     for v in graph[c]:
-        if visited[v]: continue
+        if distance[v] <= distance[c] + 1: continue
         distance[v] = min(distance[c] + 1, distance[v])
-        visited[v] = 1
         q.append(v)
 max_dist = max(distance[1:])
 cnt = distance[1:].count(max_dist)

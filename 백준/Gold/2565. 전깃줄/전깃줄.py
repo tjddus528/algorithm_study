b572d8df8a59b0379411572d9893624cbcd56780
Sungyeon <csy357@gmail.com>
2025-01-21 16:24:24 +0900

    [Gold V] Title: 전깃줄, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -5,27 +5,8 @@ data = []
 for i in range(n):
     a, b = map(int, input().split())
     data.append([a,b])
-
 data.sort()
-
-def bs(left, right, target):
-    while left < right:
-        mid = (left + right) // 2
-        if lst[mid] < target: left = mid + 1
-        else: right = mid
-    return right
-cnt = 0
-i, j = 1, 0
-lst = [0 for _ in range(n)]
-lst[0] = data[0][1]
-while i < n:
-    if lst[j] < data[i][1]:
-        lst[j+1] = data[i][1]
-        j += 1
-    else:
-        idx = bs(0, j, data[i][1])
-        lst[idx] = data[i][1]
-        cnt += 1
-    i += 1
-
-print(cnt)
+dp = [0 for _ in range(501)]
+for a, b in data:
+    dp[b] = max(dp[:b]) + 1
+print(n-max(dp))

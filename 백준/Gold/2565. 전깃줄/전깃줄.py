21f5164c68c1d09f0fabe8b3473a62d4d9271d1f
Sungyeon <csy357@gmail.com>
2025-01-21 16:25:58 +0900

    [Gold V] Title: 전깃줄, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.py
+++ 백준/Gold/2565. 전깃줄/전깃줄.py
@@ -1,11 +1,7 @@
 import sys
 input = sys.stdin.readline
 n = int(input())
-data = []
-for i in range(n):
-    a, b = map(int, input().split())
-    data.append([a,b])
-data.sort()
+data = sorted([list(map(int, input().split())) for _ in range(n)])
 dp = [0 for _ in range(501)]
 for a, b in data:
     dp[b] = max(dp[:b]) + 1

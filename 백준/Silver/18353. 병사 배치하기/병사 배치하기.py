89b41d1115dbaecf4fff9d911c175cd2878a7da1
Sungyeon <csy357@gmail.com>
2025-01-21 15:02:36 +0900

    [Silver II] Title: 병사 배치하기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -2,10 +2,26 @@ import sys
 input = sys.stdin.readline
 n = int(input())
 data = list(map(int, input().split()))
-length = [1 for _ in range(n)]
-for k in range(n):
-    for i in range(k):
-        if data[i] > data[k]:
-            length[k] = max(length[k], length[i]+1)
 
-print(n - max(length))
+def bs(l, r, target):
+    while l <= r:
+        mid = (l + r) // 2
+        if lst[mid] > target: l=mid+1
+        else: r=mid-1
+    return l
+
+cnt = 0
+i, j = 1, 0
+lst = [0 for _ in range(n)]
+lst[0] = data[0]
+while i < n:
+    if lst[j] > data[i]:
+        lst[j+1] = data[i]
+        j += 1
+    else:
+        idx = bs(0, j, data[i])
+        lst[idx] = data[i]
+        cnt += 1
+    i += 1
+
+print(cnt)

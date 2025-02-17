045aa498e9845960a7ca7c868a7d65d8ea56acc2
Sungyeon <csy357@gmail.com>
2025-02-17 18:45:24 +0900

    [level 1] Title: 폰켓몬, Time: 4.79 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,11 +2,8 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int num: nums)
-            map.put(num, map.getOrDefault(num, 0)+1);
-        int n = nums.length;
-        if (map.size() >= n/2) return n/2;
-        else return map.size();
+        Set<Integer> set = new HashSet<>();
+        for(int num: nums) set.add(num);
+        return (nums.length/2 > set.size()) ? set.size() : nums.length/2;
     }
 }

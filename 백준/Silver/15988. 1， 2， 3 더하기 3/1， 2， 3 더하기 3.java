032e6197adacb74c4e1ac9933d38d3331af90f15
Sungyeon <csy357@gmail.com>
2025-02-19 16:05:56 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 136 ms, Memory: 22220 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    long[] dp = new long[1000001];
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i = 4; i <= 1000000; i++) {
+      dp[i] += (dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009;
+    }
+    int test = Integer.parseInt(br.readLine());
+    for(int t = 0; t<test; t++) {
+      int n = Integer.parseInt(br.readLine());
+      System.out.println(dp[n]);
+    }
+  }
+}
